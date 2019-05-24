3ae1ffeafcfaca566bf53cb07838f1c1db064154
陈畅 <chench.xian@gmail.com>
2019-05-24 11:37:56 +0800

    wip: 隐患处理等功能开做
    隐患处理等功能开做

--- src/app/routes/check/check-routing.module.ts
+++ src/app/routes/check/check-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core'
 import { Routes, RouterModule } from '@angular/router'
 import { CheckItemConfigComponent } from './item-config/item-config.component'
+import { CheckRecordComponent } from './record/record.component'
+import { CheckReviewComponent } from './review/review.component'
 
-const routes: Routes = [{ path: 'item-config', component: CheckItemConfigComponent }]
+const routes: Routes = [
+  { path: 'item-config', component: CheckItemConfigComponent },
+  { path: 'record', component: CheckRecordComponent },
+  { path: 'review', component: CheckReviewComponent }
+]
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/routes/check/check.module.ts
+++ src/app/routes/check/check.module.ts
@@ -3,8 +3,10 @@ import { SharedModule } from '@shared'
 import { CheckRoutingModule } from './check-routing.module'
 import { CheckItemListComponent } from './item-list/item-list.component'
 import { CheckItemConfigComponent } from './item-config/item-config.component'
+import { CheckRecordComponent } from './record/record.component'
+import { CheckReviewComponent } from './review/review.component'
 
-const COMPONENTS = []
+const COMPONENTS = [CheckRecordComponent, CheckReviewComponent]
 const COMPONENTS_NOROUNT = [CheckItemListComponent, CheckItemConfigComponent]
 
 @NgModule({
