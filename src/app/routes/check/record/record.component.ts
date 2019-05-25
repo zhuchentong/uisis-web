1b39755b6c39597a9443db9fe92acf8e16c42f1e
陈畅 <chench.xian@gmail.com>
2019-05-25 11:57:06 +0800

    wip: 检查记录列表
    检查记录列表

--- src/app/routes/check/record/record.component.ts
+++ src/app/routes/check/record/record.component.ts
@@ -7,6 +7,7 @@ import { NzMessageService, NzModalService } from 'ng-zorro-antd'
 import { CheckLabService } from 'app/services/check-lab.service'
 import { CheckRecordModel } from 'app/model/check-record.model'
 import { Model } from 'app/model'
+import { DatePipe } from '@angular/common'
 
 @Component({
   selector: 'app-check-record',
@@ -32,13 +33,22 @@ export class CheckRecordComponent implements OnInit {
 
   public columns: STColumn[] = [
     { title: '实验室', index: 'laboratory.name' },
-    { title: '编号', index: 'checkItemConfigSnapshot.itemNumber' },
-    { title: '风险等级', index: 'checkItemConfigSnapshot.risk_level' },
+    { title: '状态', index: 'checkRecordState', format: x => this.dictPipe.transform(x.checkRecordState) },
+    { title: '检查类型', index: 'checkType', format: x => this.dictPipe.transform(x.checkType) },
+
+    { title: '创建人', index: 'creator.realName' },
+
+    { title: '创建时间', index: 'createTime', format: x => this.datePipe.transform(x.createTime, 'yyyy-MM-dd HH:mm') },
+
+    { title: '更新人', index: 'updater.realName' },
+
+    { title: '更新时间', index: 'updateTime', format: x => this.datePipe.transform(x.createTime, 'yyyy-MM-dd HH:mm') },
+
     {
       title: '操作',
       fixed: 'right',
       buttons: [
-        // { text: '修改', type: 'modal', click: x => this.modify(x) },
+        { text: '查看', type: 'modal' }
         // { text: '删除', type: 'modal', click: x => this.delete(x) }
       ]
     }
@@ -46,6 +56,7 @@ export class CheckRecordComponent implements OnInit {
 
   constructor(
     private dictPipe: DictPipe,
+    private datePipe: DatePipe,
     private checkLabService: CheckLabService,
     private modalService: NzModalService,
     public pageService: PageService,
@@ -59,7 +70,12 @@ export class CheckRecordComponent implements OnInit {
   public query() {
     this.checkLabService.queryDanger(this.pageService).subscribe(data => {
       this.dataSet = data
-      console.log(222, this.dataSet)
     })
+  }
+
+  public change(e) {
+    if (this.pageService.change(e)) {
+      this.query()
+    }
   }
 }
